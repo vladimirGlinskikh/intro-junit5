46485ae9e0888badb988ba1fe3348dcb9cad2969
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2021-01-02 02:21:50 +0600

    JUnit 5 test instance lifecycle demo

--- src/test/java/kz/zhelezyaka/GreetingTest.java
+++ src/test/java/kz/zhelezyaka/GreetingTest.java
@@ -1,15 +1,20 @@
 package kz.zhelezyaka;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
     private Greeting greeting;
 
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("Before - I am only called once!");
+    }
+
     @BeforeEach
     void setUp() {
+        System.out.println("In BeforeEach ...");
         greeting = new Greeting();
     }
 
@@ -21,5 +26,15 @@ class GreetingTest {
     @Test
     void testHelloJava() {
         System.out.println(greeting.helloJava("Vladimir"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In AfterEach ...");
+    }
+
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("After - I am only called once!");
     }
 }
